1b54941be6e837788df9e2653a6d3d43bc5a5ad3
Falphir <73762798+Falphir@users.noreply.github.com>
2022-06-06 10:58:18 +0100

    start

---
+++ src/setupTests.js
@@ -0,0 +1,10 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom';
+import enableHooks from 'jest-react-hooks-shallow';
+// pass an instance of jest to `enableHooks()`enableHooks(jest);
+
+enableHooks(jest);
+jest.setTimeout(20000)
